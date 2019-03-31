c46879281b189432f5621f58ea1608e319b05c6b
Darshan Bhatta <bhatta.darshan26@gmail.com>
2019-03-31 17:51:19 -0500

    hmmm

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget{
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _ProductCreatePage();
+  }
+}
+
+class _ProductCreatePage extends State<ProductCreatePage> {
+  String title = '';
+  String description = '';
+  double price;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(child: RaisedButton(child: Text('Save'),onPressed: (){
-      showModalBottomSheet(context: context, builder: (context){return Center(child: Text('This is a modal'),);});
-    }));
+    return Column(
+      children: <Widget>[
+        TextField(
+          onChanged: (String value) {
+            setState(() {
+              title = value;
+            });
+          },
+        ),
+        TextField(
+          maxLines: 4,
+          onChanged: (String value) {
+            setState(() {
+              description = value;
+            });
+          },
+        ),
+        TextField(
+          keyboardType: TextInputType.number,
+          onChanged: (String value) {
+            setState(() {
+              price = double.parse(value);
+            });
+          },
+        )
+      ],
+    );
   }
-
 }
